0817726b83bb1346d6d48f0cb0c7832db2b69260
rabobahago <rabobahago@gmail.com>
2021-03-07 21:30:19 +0100

    chat engine setup added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+import './App.css'
 const App = () => {
   return (
-    <div>
-      <h1>Application</h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="0669c1e6-7feb-4de1-bbdd-3797671a4715"
+      userName="rabobahago@gmail.com"
+      userSecret="u07me1030"
+    />
   )
 }
 export default App
