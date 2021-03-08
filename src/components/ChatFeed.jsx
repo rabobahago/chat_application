7c7bc77920a74f920be08e9f3eb2ebfa807f4f43
rabobahago <rabobahago@gmail.com>
2021-03-08 22:30:55 +0100

    chat feed component code added

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,7 +4,19 @@ import TheirMessage from './TheirMessage'
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props
   const chat = chats && chats[activeChat]
-  console.log(chat, userName, messages)
+  const renderMessages = (props) => {
+    const keys = Object.keys(messages)
+    return keys.map((key, index) => {
+      const message = messages[key]
+      const lastMessageKey = index === 0 ? null : keys[index - 1]
+      const isMyMessage = userName === message.render.username
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block"></div>
+        </div>
+      )
+    })
+  }
   return <div>Chat Feed</div>
 }
 export default ChatFeed
