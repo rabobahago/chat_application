586c77fbe144c0b4e78f2180df332e153d602120
rabobahago <rabobahago@gmail.com>
2021-03-13 12:57:48 +0100

    message form logic added

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,4 +1,25 @@
-const MessageForm = () => {
-  return <div>MessageForm</div>
+import { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
+const MessageForm = (props) => {
+  const [value, setValue] = useState('')
+  const { chatId, creds } = props
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+  const handleChange = (event) => {
+    setValue(event.target.value)
+    isTyping(props, chatId)
+  }
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </form>
+  )
 }
 export default MessageForm
